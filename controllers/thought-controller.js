57a24325e8f7e0064541a4be7ddee288a91bb3df
TraBee <tra8brown@gmail.com>
2021-09-09 20:05:31 -0600

    server.js

---
+++ controllers/thought-controller.js
@@ -0,0 +1,11 @@
+const { Thought, User } = require("../models");
+
+//GET all thoughts
+
+//create a thought via POST method
+
+//GET a Thought by :id
+
+//update thoughts reaction by :id
+
+//delete thoughts by :id
---
+++ controllers/user-controller.js
@@ -0,0 +1,16 @@
+const { User, Thought } = require("../models");
+
+//GET all users
+const userController = {},
+
+    //create user via POST method
+
+    //GET user by :id
+
+    //Udate User by :id
+
+    //DELETE user by :id
+
+    //Updat4e a friend
+
+    //deleting a friend
